3172f3ce6ab5624fa3496cd9de65b8aaf02ef867
Garnik <garnik645@gmail.com>
2022-10-09 17:41:08 +0400

    domain functionality was added

--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,20 @@
 import argparse
 import sys
+import os
 import json
 
 
-class Break:
+class BreakTemplate:
     HOUR_TO_SECONDS = 3600
     MINUTE_TO_SECONDS = 60
     
+    @staticmethod
+    def is_valid_interval(interval, class_start_in_seconds, class_end_in_seconds):
+        return interval[0] > class_start_in_seconds and interval[1] < class_end_in_seconds
+    
     @classmethod
     def convert_time_to_seconds(cls, time):
-        split_time = time.split()
+        split_time = time.split(':')
         return int(split_time[0]) * cls.HOUR_TO_SECONDS + \
         int(split_time[1]) * cls.MINUTE_TO_SECONDS + int(split_time[2])
     
@@ -19,15 +24,42 @@ class Break:
         self.is_relative_to_start = True if json_template['start_time_type'] == 'RELATIVE_TO_CLASS_START' else False
         for start_time in json_template['start_times']:
             self.start_times.append(self.convert_time_to_seconds(start_time))
+            
+    def get_break_intervals(self, class_start, class_end):
+        intervals = []
+        class_start_in_seconds = self.convert_time_to_seconds(class_start)
+        class_end_in_seconds = self.convert_time_to_seconds(class_end)
+        for break_start in self.start_times:
+            if self.is_relative_to_start:
+                interval = (class_start_in_seconds + break_start, class_start_in_seconds + break_start + self.duration)
+            else:
+                interval = (class_end_in_seconds - break_start, class_end_in_seconds - break_start + self.duration)
+            
+            if self.is_valid_interval(interval, class_start_in_seconds, class_end_in_seconds):
+                intervals.append(interval)
+        return intervals
+                
 
 
-def read_from_json(args):
-    with open(args.data_path) as json_file:
-        json_data = json.load(json_file)
-        span_data = json_data[args.span_id]
-        breaks = []
-        for template in span_data:
-            breaks.append(Break(template))
+class Domain:
+    def __init__(self, args):
+        self.args = args
+        self.break_intervals = []
+    
+    def collect_template_intervals(self):
+        with open(self.args.data_path) as data_file:
+            data_json = json.load(data_file)
+            span_json = data_json[self.args.span_id]
+            for template_json in span_json:
+                template_obj = BreakTemplate(template_json)
+                intervals = template_obj.get_break_intervals(self.args.start_time, self.args.end_time)
+                self.break_intervals.append(intervals)
+
+
+# todo check time format
+def check_args(args):
+    if not os.path.isfile(args.data_path):
+        raise FileNotFoundError("File with path {} wasn't found!".format(args.data_path))
 
 
 def parse_args(argv):
@@ -35,8 +67,8 @@ def parse_args(argv):
         description='Break scheduler for university classes')
     
     required_args = {
-        '--start-time': 'Start time for classes',
-        '--end-time': 'End time for classes',
+        '--start-time': 'Start time for classes, in this format - HH:MM:SS',
+        '--end-time': 'End time for classes, in this format - HH:MM:SS',
         '--data-path': 'Path to data.json file',
         '--span-id': 'Break template id in data.json file'
     }
@@ -44,11 +76,15 @@ def parse_args(argv):
     for flag, info in required_args.items():
         parser.add_argument(flag, help=info, required=True)
 
-    return parser.parse_args(argv)
+    args = parser.parse_args(argv)
+    check_args(args)
+    return args
 
 
 def main(args):
-    print(args.start_time)
+    domain = Domain(args)
+    domain.collect_template_intervals()
+    print(domain.break_intervals)
 
 
 if __name__ == '__main__':
