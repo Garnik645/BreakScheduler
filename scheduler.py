0f716d38814b0cb20bc82d7e2f47e8cf40ac6dd5
Garnik Khroyan <garnik645@gmail.com>
2022-10-13 18:17:22 +0400

    further modifications

--- scheduler.py
+++ scheduler.py
@@ -3,16 +3,23 @@ import sys
 import os
 import re
 import json
+from dataclasses import dataclass
+from itertools import product
 
-"""
 
-class BreakTemplate:
+@dataclass
+class BreakInterval:
+    begin_sec: int
+    end_sec: int
+
+
+class BreakTemplateParser:
     HOUR_TO_SECONDS = 3600
     MINUTE_TO_SECONDS = 60
     
     @staticmethod
     def is_valid_interval(interval, class_start_in_seconds, class_end_in_seconds):
-        return interval[0] > class_start_in_seconds and interval[1] < class_end_in_seconds
+        return interval.begin_sec > class_start_in_seconds and interval.end_sec < class_end_in_seconds
     
     @classmethod
     def convert_time_to_seconds(cls, time):
@@ -20,43 +27,40 @@ class BreakTemplate:
         return int(split_time[0]) * cls.HOUR_TO_SECONDS + \
         int(split_time[1]) * cls.MINUTE_TO_SECONDS + int(split_time[2])
     
-    def __init__(self, json_template):
-        self.start_times = []
-        self.duration = int(json_template['break_duration']) * self.MINUTE_TO_SECONDS
-        self.is_relative_to_start = True if json_template['start_time_type'] == 'RELATIVE_TO_CLASS_START' else False
-        for start_time in json_template['start_times']:
-            self.start_times.append(self.convert_time_to_seconds(start_time))
-            
-    def get_break_intervals(self, class_start, class_end):
+    def __init__(self, template_json, class_begin_time, class_end_time):
+        self.class_start_in_seconds = self.convert_time_to_seconds(class_begin_time)
+        self.class_end_in_seconds = self.convert_time_to_seconds(class_end_time)
+        self.is_relative_to_start = True if template_json["start_time_type"] == "RELATIVE_TO_CLASS_START" else False
+        self.start_times = template_json["start_times"]
+        self.duration = template_json["break_duration"]
+
+    def construct_valid_interval_list(self):
         intervals = []
-        class_start_in_seconds = self.convert_time_to_seconds(class_start)
-        class_end_in_seconds = self.convert_time_to_seconds(class_end)
-        for break_start in self.start_times:
+        for break_start_time in self.start_times:
+            break_start_in_seconds = self.convert_time_to_seconds(break_start_time)
             if self.is_relative_to_start:
-                interval = (class_start_in_seconds + break_start, class_start_in_seconds + break_start + self.duration)
+                interval = BreakInterval(self.class_start_in_seconds + break_start_in_seconds,
+                                         self.class_start_in_seconds + break_start_in_seconds + self.duration)
             else:
-                interval = (class_end_in_seconds - break_start, class_end_in_seconds - break_start + self.duration)
-            
-            if self.is_valid_interval(interval, class_start_in_seconds, class_end_in_seconds):
+                interval = BreakInterval(self.class_end_in_seconds - break_start_in_seconds,
+                                         self.class_end_in_seconds - break_start_in_seconds + self.duration)
+            if self.is_valid_interval(interval, self.class_start_in_seconds, self.class_end_in_seconds):
                 intervals.append(interval)
         return intervals
-                
 
 
 class Domain:
-    def __init__(self, args):
-        self.args = args
-        self.break_intervals = []
-    
-    def collect_template_intervals(self):
-        with open(self.args.data_path) as data_file:
-            data_json = json.load(data_file)
-            span_json = data_json[self.args.span_id]
-            for template_json in span_json:
-                template_obj = BreakTemplate(template_json)
-                intervals = template_obj.get_break_intervals(self.args.start_time, self.args.end_time)
-                self.break_intervals.append(intervals)
-"""
+    def __init__(self, span_json, class_begin_time, class_end_time):
+        self.templates = []
+        for template_json in span_json:
+            parser = BreakTemplateParser(template_json, class_begin_time, class_end_time)
+            self.templates.append(parser.construct_valid_interval_list())
+
+def get_span_json(data_path, span_id):
+    with open(data_path) as data_file:
+        data_json = json.load(data_file)
+    return data_json[span_id]
+
 
 def is_time_valid(time_string):
     if len(time_string) != len("HH:MM:SS"):
@@ -70,26 +74,29 @@ def is_time_valid(time_string):
     if hour >= 24:
         return False
     
-    return True
-    
+    return True    
 
 
 def check_args(args):
     if not os.path.isfile(args.data_path):
         raise FileNotFoundError(f"File with path {args.data_path} wasn't found!")
+    
     for time_arg in [args.start_time, args.end_time]:
         if not is_time_valid(time_arg):
-            raise ValueError(f"argument '{time_arg}' has invalid time format! Valid example is 23:59:59.")
+            raise ValueError(f"argument '{time_arg}' has invalid time format! Valid example is '23:59:59'.")
+        
+    if args.start_time > args.end_time:
+        raise ValueError("Given time range is invalid!")
 
 
 def parse_args(argv):
     parser = argparse.ArgumentParser(description='Break scheduler for university classes')
     
     required_args = {
-        '--start-time': 'Start time for classes, in the following format - HH:MM:SS',
-        '--end-time': 'End time for classes, in the following format - HH:MM:SS',
-        '--data-path': 'Path to data.json file',
-        '--span-id': 'Break template id in data.json file'
+        "--start-time": "Start time for classes, in the following format - HH:MM:SS",
+        "--end-time": "End time for classes, in the following format - HH:MM:SS",
+        "--data-path": "Path to data.json file",
+        "--span-id": "Break template id in data.json file"
     }
     
     for flag, info in required_args.items():
@@ -101,8 +108,16 @@ def parse_args(argv):
 
 
 def main(args):
-    pass
+    domain = Domain(get_span_json(args.data_path, args.span_id), args.start_time, args.end_time)
+    """
+    initial_condition = 2 * Domain.HOUR_TO_SECONDS
+    result = []
+    while not result and initial_condition >= 0:
+        result = Domain.get(initial_condition)
+        initial_condition -= 15 * Domain.MINUTE_TO_SECONDS
+    
+    print(result)
+    """
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main(parse_args(sys.argv[1:]))
